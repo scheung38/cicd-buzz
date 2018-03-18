5f8db8a6d6114dde347e0dffbecde5992b697be8
Sebastian Cheung CQF <sebastian_cheung@yahoo.com>
2018-03-18 18:46:15 +0100

    Add New Test

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -18,6 +18,15 @@ def test_sample_multiple_words():
     assert words[0] is not words[1]
 
 
+def test_sample_multiple_letters():
+    l = ('foo', 'bar', 'foobar')
+    words = generator.sample(l, 2)
+    assert len(words) == 2
+    assert words[0] in l
+    assert words[1] in l
+    assert words[0] is not words[1]
+
+
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
     assert len(phrase.split()) >= 5
